04173c433e83cd15fb0bf9352fd1808d1beb773b
Divya <basanidivya2@gmail.com>
2024-01-01 18:32:13 +0530

    added javascript

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+var toys = "inside we have toys";
+var books = "inside we have books";
+var clothes = "inside we have clothes";
+
+console.log(books);
+
+//1. CREATE A VARIABLE
+let myName = "Divya";
+console.log(myName);
+
+// 2. NUMBER CRUNCHING
+let num1 = 8;
+let num2 = 4;
+let sum = num1+num2;
+console.log(sum);
+
+// 3.STRING CONCATENATION
+let firstName = "Divya";
+let lastName = "Basani"
+let fullName = firstName + "" + lastName
+console.log(fullName);
+
+// 4.Boolean Logic
+let isRaining = true;
+console.log(isRaining)
+let isDancing = false;
+console.log(isDancing)
+
+// 5.Update Values
+let counter = 23;
+counter++;
+console.log(counter);
+
+// 6.CONSTANTS
+const daysInMonth = 30;
+console.log(daysInMonth);
+const myAge = 24;
+console.log(myAge);
+
+// 7.PROMPT USER INPUT
+let userAge = prompt("how old are you?");
+prompt
+console.log(userAge);
+
+// 8.Temperature Conversion
+let celsius = 25;
+let fahrenheit = celsius * 9/5 + 32;
+console.log(fahrenheit);
+
+// let age =12;
+// if (age >= 13) {
+//     console.log("you are teenager");
+// }else {
+//     console.log("you are not a teenager");
+// }
