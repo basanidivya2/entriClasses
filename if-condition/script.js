32cd81e7c4925273f48237fe3e97ab7de2077f8c
Divya <basanidivya2@gmail.com>
2024-01-04 13:53:00 +0530

    added script.js

---
+++ if-condition/script.js
@@ -0,0 +1,36 @@
+//1.Checking if a number is positive:
+let age =12;
+if (age >= 13) {
+    console.log("you are teenager");
+}else {
+    console.log("you are not a teenager");
+}
+//2.Checking if a number is even:
+let number = 8;
+if (number > 0) {
+    console.log("The number is positive");
+} else {
+    console.log("The number is not positive")
+}
+//3.Checking if a string is empty:
+let str="";
+if (str==="") {
+    console.log("The string is empty");
+} else {
+    console.log("The string is not empty");
+}
+// 4.Checking if a user is logged in:
+let isLoggedIn = false;
+if (isLoggedIn) {
+    console.log("User is logged in");
+} else {
+    console.log("User is not logged in");
+}
+// 5.Checking if a variable is defined:
+let 
+
+
+// 8.Temperature Conversion
+let celsius = 25;
+let fahrenheit = celsius * 9/5 + 32;
+console.log(fahrenheit);
